d71d640e12f1e369c08d0de8665b7d6b2036c555
Bikcode <bikcodeh@gmail.com>
2022-01-16 12:37:56 -0500

    test all heroes and root endpoint

--- src/test/kotlin/com/bikcode/ApplicationTest.kt
+++ src/test/kotlin/com/bikcode/ApplicationTest.kt
@@ -1,25 +1,144 @@
 package com.bikcode
 
-import io.ktor.routing.*
+import com.bikcode.models.ApiResponse
+import com.bikcode.models.Hero
+import com.bikcode.repository.HeroRepository
+import com.bikcode.repository.NEXT_PAGE_KEY
+import com.bikcode.repository.PREV_PAGE_KEY
 import io.ktor.http.*
-import io.ktor.serialization.*
-import io.ktor.features.*
-import org.slf4j.event.*
-import io.ktor.request.*
-import io.ktor.application.*
-import io.ktor.response.*
 import kotlin.test.*
 import io.ktor.server.testing.*
-import com.bikcode.plugins.*
+import io.ktor.application.*
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import org.koin.java.KoinJavaComponent.inject
+import kotlin.math.exp
 
 class ApplicationTest {
+
+    private val heroRepository: HeroRepository by inject(HeroRepository::class.java)
+
     @Test
-    fun testRoot() {
-        withTestApplication({ configureRouting() }) {
+    fun `access root endpoint assert correct information`() {
+        withTestApplication(moduleFunction = Application::module) {
             handleRequest(HttpMethod.Get, "/").apply {
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("Hello World!", response.content)
+                assertEquals(
+                    expected = HttpStatusCode.OK,
+                    actual = response.status()
+                )
+                assertEquals(
+                    expected = "Welcome to Boruto server",
+                    actual = response.content
+                )
             }
         }
+    }
+
+    @Test
+    fun `access all heroes endpoint assert correct information`() {
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/boruto/heroes").apply {
+                assertEquals(
+                    expected = HttpStatusCode.OK,
+                    actual = response.status()
+                )
+
+                val expected = ApiResponse(
+                    success = true,
+                    message = "OK",
+                    prevPage = null,
+                    nextPage = 2,
+                    heroes = heroRepository.page1
+                )
+
+                val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+                assertEquals(
+                    expected = expected,
+                    actual = actual
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `access all heroes endpoint, query second page assert correct information`() {
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/boruto/heroes?page=2").apply {
+                assertEquals(
+                    expected = HttpStatusCode.OK,
+                    actual = response.status()
+                )
+
+                val expected = ApiResponse(
+                    success = true,
+                    message = "OK",
+                    prevPage = 1,
+                    nextPage = 3,
+                    heroes = heroRepository.page2
+                )
+
+                val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+                assertEquals(
+                    expected = expected,
+                    actual = actual
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `access all heroes endpoint, query all pages assert correct information`() {
+        withTestApplication(moduleFunction = Application::module) {
+            val pages = 1..5
+            val heroes = listOf(
+                heroRepository.page1,
+                heroRepository.page2,
+                heroRepository.page3,
+                heroRepository.page4,
+                heroRepository.page5
+            )
+
+            pages.forEach { page ->
+                handleRequest(HttpMethod.Get, "/boruto/heroes?page=$page").apply {
+                    assertEquals(
+                        expected = HttpStatusCode.OK,
+                        actual = response.status()
+                    )
+                    val expected = ApiResponse(
+                        success = true,
+                        message = "OK",
+                        prevPage = calculatePage(page)["prevPage"],
+                        nextPage = calculatePage(page)["nextPage"],
+                        heroes = heroes[page - 1]
+
+                    )
+                    assertEquals(
+                        expected = expected,
+                        actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+                    )
+                }
+            }
+        }
+    }
+    private fun calculatePage(page: Int): Map<String, Int?> {
+        var prevPage: Int? = page
+        var nextPage: Int? = page
+
+        if (page in 1..4) {
+            nextPage = nextPage?.plus(1)
+        }
+
+        if (page in 2..5) {
+            prevPage = prevPage?.minus(1)
+        }
+
+        if (page == 1) {
+            prevPage = null
+        }
+
+        if (page == 5) {
+            nextPage = null
+        }
+        return mapOf(PREV_PAGE_KEY to prevPage, NEXT_PAGE_KEY to nextPage)
     }
 }
